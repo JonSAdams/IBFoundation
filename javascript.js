ffe89f2155712d3c463574a9f47eab6503e670db
Jonathan Adams <jonsadams@icloud.com>
2023-12-19 11:05:27 -0500

    Update javascript

--- javascript.js
+++ javascript.js
@@ -1,13 +1,18 @@
 function openNamingConventions() {
-    let x =document.getElementById("openConventions");
-    if(x.style.display === "none") {
+    let x = document.getElementById("openConventions");
+    if (x.style.display === "none") {
         x.style.display = "block";
+        x.style.transition = "display 0.5s ease-in-out";
     } else {
-        x.style.display = "none"
+        x.style.display = "none";
+        x.style.transition = "display 0.5s ease-in-out";
     }
 }
 
+
 function copyTheText() {
     var copyText = document.getElementById('string-of-text').innerHTML;
-    navigator.clipboard.writeText(copyText);
-    };
+    navigator.clipboard.writeText(copyText).then(() => {
+        alert('Text copied to clipboard');
+    });
+}
